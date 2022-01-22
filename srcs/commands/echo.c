004bbdd2c7ab3a7c6edf1f2c06a6919138ce5a8e
AntoninLiens <antoninliens@hotmail.be>
2022-01-22 21:20:58 +0100

    plus gerer les quotes dans echo

--- srcs/commands/echo.c
+++ srcs/commands/echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ctirions <ctirions@student.s19.be>         +#+  +:+       +#+        */
+/*   By: aliens <aliens@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 23:19:42 by ctirions          #+#    #+#             */
-/*   Updated: 2022/01/17 18:05:24 by ctirions         ###   ########.fr       */
+/*   Updated: 2022/01/22 21:19:46 by aliens           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,17 +36,8 @@ int	echo(char **cmd)
 		no_backslash = 1;
 		i++;
 	}
-
 	if (!cmd[i])
 		return (0);
-    if (cmd[i][0] == '\"')
-    {
-    	cmd[i]++;
-		tmp = i - 1;
-		while (cmd[++tmp])
-			if (cmd[tmp][ft_strlen(cmd[tmp]) - 1] == '\"')
-				cmd[tmp][ft_strlen(cmd[tmp]) - 1] = '\0';
-    }
     while (cmd[i])
 	{
     	printf("%s", cmd[i++]);
