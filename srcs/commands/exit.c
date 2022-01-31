c63b2f778fa6154f521ccce1e636c9ccb6da89ec
AntoninLiens <antoninliens@hotmail.be>
2022-01-31 16:01:51 +0100

    better exit

--- srcs/commands/exit.c
+++ srcs/commands/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ctirions <ctirions@student.s19.be>         +#+  +:+       +#+        */
+/*   By: aliens <aliens@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 16:36:01 by ctirions          #+#    #+#             */
-/*   Updated: 2022/01/24 18:32:57 by ctirions         ###   ########.fr       */
+/*   Updated: 2022/01/31 10:54:49 by aliens           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,18 @@ int	mini_exit(t_mini *shell, char **cmd)
 {
 	int	i;
 
+	if (cmd[2])
+	{
+		printf("minishell: exit: too many argument\n");
+		return (2);
+	}
 	if (!cmd[1])
 	{
 		shell->exit = 1;
 		return (0);
 	}
 	i = 0;
-	while (cmd[1][i] && ft_isdigit(cmd[1][i]))
+	while (cmd[1][i] && (ft_isdigit(cmd[1][i]) || cmd[1][i] == '-'))
 		i++;
 	if (cmd[1][i])
 	{
